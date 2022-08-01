480ab6f47cae1a84379d0d6b742e3faf76ec1550
Ashura <adagbesenam@gmail.com>
2022-08-01 00:37:12 +0000

    Minor bug fixes

--- src/main/java/com/itconsortiumgh/ObservabilityAnnotation/CustomAnnotationApplication.java
+++ src/main/java/com/itconsortiumgh/ObservabilityAnnotation/CustomAnnotationApplication.java
@@ -1,7 +1,6 @@
 package com.itconsortiumgh.ObservabilityAnnotation;
 
-import com.itconsortiumgh.ObservabilityAnnotation.test.TestController;
-import org.springframework.boot.CommandLineRunner;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
